79dffe498388a9ffe40d39298487bfadf28b3494
Onurcan Sever <onsever@icloud.com>
2022-10-26 14:18:15 -0400

    finished carousel component

--- components/ui/Carousel.jsx
+++ components/ui/Carousel.jsx
@@ -1,8 +1,30 @@
 import Image from "next/image";
 import React from "react";
 import Title from "./Title";
+import Slider from "react-slick";
 
 const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    appenDots: (dots) => {
+      return (
+        <div>
+          <ul>{dots}</ul>
+        </div>
+      );
+    },
+    customPaging: (i) => {
+      return <div className="w-3 h-3 border bg-white rounded-full mt-10"></div>;
+    },
+  };
+
   return (
     <div className="h-screen w-full container mx-auto -mt-[88px]">
       <div className="absolute top-0 left-0 w-full h-full">
@@ -15,16 +37,44 @@ const Carousel = () => {
           />
         </div>
       </div>
-      <div className="relative container mx-auto text-white top-48 flex flex-col items-start gap-y-10">
-        <Title className="text-6xl">Fast Food Restaurant</Title>
-        <p className="text-sm w-1/2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi,
-          recusandae blanditiis voluptas, sed ipsum magni deleniti cum incidunt
-          soluta velit quod ratione fuga temporibus eligendi delectus atque
-          laudantium impedit.
-        </p>
-        <button className="btn-primary">Order Now</button>
-      </div>
+      <Slider {...settings}>
+        <div>
+          <div className="mt-48 container mx-auto text-white flex flex-col items-start gap-y-10">
+            <Title className="text-6xl">Fast Food Restaurant</Title>
+            <p className="text-sm w-1/2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
+              modi, recusandae blanditiis voluptas, sed ipsum magni deleniti cum
+              incidunt soluta velit quod ratione fuga temporibus eligendi
+              delectus atque laudantium impedit.
+            </p>
+            <button className="btn-primary">Order Now</button>
+          </div>
+        </div>
+        <div>
+          <div className="mt-48 container mx-auto text-white flex flex-col items-start gap-y-10">
+            <Title className="text-6xl">Fast Food Restaurant</Title>
+            <p className="text-sm w-1/2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
+              modi, recusandae blanditiis voluptas, sed ipsum magni deleniti cum
+              incidunt soluta velit quod ratione fuga temporibus eligendi
+              delectus atque laudantium impedit.
+            </p>
+            <button className="btn-primary">Order Now</button>
+          </div>
+        </div>
+        <div>
+          <div className="mt-48 container mx-auto text-white flex flex-col items-start gap-y-10">
+            <Title className="text-6xl">Fast Food Restaurant</Title>
+            <p className="text-sm w-1/2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
+              modi, recusandae blanditiis voluptas, sed ipsum magni deleniti cum
+              incidunt soluta velit quod ratione fuga temporibus eligendi
+              delectus atque laudantium impedit.
+            </p>
+            <button className="btn-primary">Order Now</button>
+          </div>
+        </div>
+      </Slider>
     </div>
   );
 };
