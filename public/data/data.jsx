e7b768c3974dbd305fb205e7876b66dab4deef31
Onurcan Sever <onsever@icloud.com>
2022-10-26 17:13:44 -0400

    test data for the components

---
+++ public/data/data.jsx
@@ -0,0 +1,32 @@
+export const carouselData = [
+  {
+    title: "Fast Food Restaurant",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi, recusandae blanditiis voluptas, sed ipsum magni deleniti cum incidunt soluta velit quod ratione fuga temporibus eligendi delectus atque laudantium impedit.",
+  },
+  {
+    title: "Fast Food Restaurant",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi, recusandae blanditiis voluptas, sed ipsum magni deleniti cum incidunt soluta velit quod ratione fuga temporibus eligendi delectus atque laudantium impedit.",
+  },
+  {
+    title: "Fast Food Restaurant",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, modi, recusandae blanditiis voluptas, sed ipsum magni deleniti cum incidunt soluta velit quod ratione fuga temporibus eligendi delectus atque laudantium impedit.",
+  },
+];
+
+export const campaignData = [
+  {
+    id: 1,
+    image: "/images/o1.jpg",
+    title: "Tasty Thursdays",
+    discount: 20,
+  },
+  {
+    id: 2,
+    image: "/images/o2.jpg",
+    title: "Tasty Thursdays",
+    discount: 15,
+  },
+];
