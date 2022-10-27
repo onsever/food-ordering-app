5f25c46171e74819e1228b05eca38204d04b816d
Onurcan Sever <onsever@icloud.com>
2022-10-27 14:31:43 -0400

    input array for inputs

---
+++ public/data/inputs.js
@@ -0,0 +1,32 @@
+export const inputs = [
+  {
+    id: 1,
+    name: "fullName",
+    type: "text",
+    placeholder: "Your Full Name",
+  },
+  {
+    id: 2,
+    name: "phoneNumber",
+    type: "tel",
+    placeholder: "Your Phone Number",
+  },
+  {
+    id: 3,
+    name: "email",
+    type: "email",
+    placeholder: "Your Email Address",
+  },
+  {
+    id: 4,
+    name: "persons",
+    type: "number",
+    placeholder: "How Many Persons",
+  },
+  {
+    id: 5,
+    name: "date",
+    type: "date",
+    placeholder: "Select a Date",
+  },
+];
