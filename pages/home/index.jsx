84d25e4afeccd5e880d09bdec3e463bd97fb4b7e
Onurcan Sever <onsever@icloud.com>
2022-10-27 14:32:00 -0400

    added booking component to the home page

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import About from "../../components/about/About";
-import Campaigns from "../../components/layout/Campaigns";
+import Input from "../../components/input/Input";
+import Campaigns from "../../components/campaign/Campaigns";
 import MenuWrapper from "../../components/menu/MenuWrapper";
 import Carousel from "../../components/ui/Carousel";
+import Booking from "../../components/booking/Booking";
 
 const Index = () => {
   return (
@@ -11,6 +13,7 @@ const Index = () => {
       <Campaigns />
       <MenuWrapper />
       <About />
+      <Booking />
     </div>
   );
 };
