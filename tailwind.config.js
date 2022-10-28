4e289282f96ec0eb2d7d53128f3f8d3c13ca0069
Onurcan Sever <onsever@icloud.com>
2022-10-27 21:09:47 -0400

    added danger color

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       colors: {
         primary: "#ffbe33",
         secondary: "#222831",
+        danger: "red",
       },
       fontFamily: {
         dancing: ["Dancing Script", "cursive"],
