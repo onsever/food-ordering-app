df27c7617e03e8140bc6ef32c757e7fbcb73fb75
Onurcan Sever <onsever@icloud.com>
2022-10-28 13:09:33 -0400

    added register validation schema

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -22,3 +22,20 @@ export const loginSchema = Yup.object({
       "Password must contain at least one uppercase, one lowercase, one number and one special character."
     ),
 });
+
+export const registerSchema = Yup.object({
+  fullName: Yup.string()
+    .required("Full name is required.")
+    .min(3, "Full name must be at least 3 characters."),
+  email: Yup.string().required("Email is required.").email("Email is invalid."),
+  password: Yup.string()
+    .required("Password is required.")
+    .min(8, "Password is too short - should be 8 characters minimum.")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Password must contain at least one uppercase, one lowercase, one number and one special character."
+    ),
+  confirmPassword: Yup.string()
+    .required("Confirm password is required.")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
