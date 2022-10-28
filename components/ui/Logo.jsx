729d811c0ad0e4924b40e1494a7c8cd731e8bb8c
Onurcan Sever <onsever@icloud.com>
2022-10-28 19:30:24 -0400

    added hover effect for logo

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/">
-      <div className="text-[2rem] font-dancing font-bold cursor-pointer">
+      <div className="text-[2rem] font-dancing font-bold cursor-pointer hover:text-primary">
         FoodLog
       </div>
     </Link>
