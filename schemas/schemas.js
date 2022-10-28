ace4d72e8012fa22d943fc63300eac8aa2f8642d
Onurcan Sever <onsever@icloud.com>
2022-10-28 13:40:08 -0400

    created admin schemas for validation

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -39,3 +39,16 @@ export const registerSchema = Yup.object({
     .required("Confirm password is required.")
     .oneOf([Yup.ref("password"), null], "Passwords must match"),
 });
+
+export const adminSchema = Yup.object({
+  username: Yup.string()
+    .required("Username is required.")
+    .min(3, "Username must be at least 3 characters."),
+  password: Yup.string()
+    .required("Password is required.")
+    .min(8, "Password is too short - should be 8 characters minimum.")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Password must contain at least one uppercase, one lowercase, one number and one special character."
+    ),
+});
