e73b6f9c56f959aae487636a42588ef72d51640a
Onurcan Sever <onsever@icloud.com>
2022-10-28 19:27:51 -0400

    added validation schemas for profile page

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -52,3 +52,27 @@ export const adminSchema = Yup.object({
       "Password must contain at least one uppercase, one lowercase, one number and one special character."
     ),
 });
+
+export const accountSchema = Yup.object({
+  fullName: Yup.string()
+      .required("Full name is required.")
+      .min(3, "Full name must be at least 3 characters."),
+  phoneNumber: Yup.string()
+      .required("Phone number is required.")
+      .min(10, "Phone number must be at least 10 characters."),
+  email: Yup.string().required("Email is required.").email("Email is invalid."),
+  address: Yup.string().required("Address is required."),
+});
+
+export const passwordSchema = Yup.object({
+  password: Yup.string()
+      .required("Password is required.")
+      .min(8, "Password is too short - should be 8 characters minimum.")
+      .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+          "Password must contain at least one uppercase, one lowercase, one number and one special character."
+      ),
+  confirmPassword: Yup.string()
+      .required("Confirm password is required.")
+      .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
